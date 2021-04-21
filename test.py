d0411840796c510ce21868963f5682a2f3c7c5e6
brilliantZhang <zhangzhihui1999314@gmail.com>
2021-04-21 10:08:09 +0800

    bug fix

--- test.py
+++ test.py
@@ -70,7 +70,7 @@ def predict(bi,mfcc):
         feed_dict[sess.graph.get_tensor_by_name('input_{}:0'.format(i+1))]=bi
 
     res = sess.run(ops, feed_dict=feed_dict)
-    st.write(tmp)
+    st.write(res)
     y = [np.argmax(r) for r in res]
     st.write(y)
     tmp = [[np.argmax(r),max(r)] for r in res]
